827ae98fe9eb366d3c300d59c9fbddff788cccd4
ady <DoveyLoveyCora@163.com>
2022-03-28 21:06:29 +0800

    feat:  p-63 为swagger的scheme添加属性信息

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 // class-validator为我们提供校验 如果参数无效 应用程序将自动响应400 BadRequest
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of coffee.' }) // 该装饰器允许我们对属性设置各种模式 例如默认值 或描述信息
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The brand of coffee.' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: [], description: 'The flavor of coffee.' })
   @IsString({ each: true }) // 确保数组的每一项都是string 期望是一个字符串的数组
   readonly flavors: string[]; // 口味
 }
