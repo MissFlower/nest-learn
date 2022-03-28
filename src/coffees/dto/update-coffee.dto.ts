5c696edc0bca633a7940a4c42b066f0bc7b5476d
ady <DoveyLoveyCora@163.com>
2022-03-28 21:00:37 +0800

    feat: 启用nest-cli 来解决swagger入参出参不展示问题

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger'; // 从@nestjs/swagger包 而不是@nestjs/mapped-types包中导入 解决swagger-ui上入参不展示问题 主要原因可能是这些都是可选属性
 import { CreateCoffeeDto } from './create-coffee.dto';
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
