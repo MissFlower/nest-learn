c1e572202598a4cc48ed4887e0da0da0cd6783b1
ady <DoveyLoveyCora@163.com>
2022-03-28 15:08:01 +0800

    feat: p-45 通过process.env配置数据

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,20 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // 从默认位置加载和解析我们的.env文件
     CoffeesModule,
     TypeOrmModule.forRoot({
       //请记住 我们只在主AppModule中使用了forRoot() 其他模块都将使用forFeature()
       type: 'postgres', // 使用数据库类型
-      host: 'localhost', // 主机 本地开发使用localhost
-      port: 5432, // 端口号 在docker-compose.yml中设置的
-      username: 'postgres', // 是访问我们数据库的用户名 这是PostgreSQL数据库的默认设置
-      password: 'pass123', // 密码 在docker-compose.yml中设置的
-      database: 'postgres', // 数据库名称
+      host: process.env.DATABASE_HOST, // 主机 本地开发使用localhost
+      port: process.env.DATABASE_PORT, // 端口号 在docker-compose.yml中设置的
+      username: process.env.DATABASE_USER, // 是访问我们数据库的用户名 这是PostgreSQL数据库的默认设置
+      password: process.env.DATABASE_PASSWORD, // 密码 在docker-compose.yml中设置的
+      database: process.env.DATABASE_NAME, // 数据库名称
       autoLoadEntities: true, // 自动加载模块 而不是制定实体数组
       synchronize: true, // 同步 确保我们的TypeORM实体在每次运行应用程序时都会与数据库同步 切记 只用于开发 生产中禁用
     }),
