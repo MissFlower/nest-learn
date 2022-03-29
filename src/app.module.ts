64a79a88adcd83071adf2c0a9c40a958a60363f5
ady <DoveyLoveyCora@163.com>
2022-03-29 22:51:41 +0800

    feat: p74-75 安装mongoose相关依赖和初始化配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { AbcController } from './modules/abc/abc.controller';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
-  controllers: [AppController, CatsController, AbcController],
+  imports: [
+    CoffeesModule,
+    // MongooseModule 第一个参数接收一个数据库连接的url（数据库所在位置） 第二个参数是一些配置项
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
